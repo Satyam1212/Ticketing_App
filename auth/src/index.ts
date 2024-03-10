1741e3e3b5bc25554753f45d8fd8922b52c70b85
Satyam1212 <satyamjadhav088@gmail.com>
2024-03-11 00:10:52 +0530

    Added errorhandler in auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 //No need of body-parser from version 4.16
-
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express()
 app.use(express.json())
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send("Hi")
-})
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+app.use(errorHandler)
 
 app.listen(3000, () => {
     console.log('Listening on 3000')
