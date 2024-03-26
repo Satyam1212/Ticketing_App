541f5296e18aa70c98f9aacb26aaa79686b3b853
Satyam1212 <satyamjadhav088@gmail.com>
2024-03-27 01:06:17 +0530

    Started working on post signup activity

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,33 @@
 import {useState} from 'react';
-import axios from 'axios';
+import  {useRequest}  from '../../hooks/use-request';
+import Router from 'next/router';
 
 const SignUp = () =>{
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    // const [errors, setErrors] = useState([]);
+    const { doRequest , errors} = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess:  () => Router.push('/')
+    })
 
     const onSubmit = async (event) =>{
         event.preventDefault();
+        await doRequest();
 
-        try{
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            })
-            console.log(response.data);
-        } catch(err){
-            console.log(err.response.data);
-            setErrors(err.response.data.errors);
-        }
+        // try{
+        //     const response = await axios.post('/api/users/signup', {
+        //         email, password
+        //     })
+        //     console.log(response.data);
+        // } catch(err){
+        //     console.log(err.response.data);
+        //     setErrors(err.response.data.errors);
+        // }
 
 
         // console.log(response.data);
@@ -35,13 +45,13 @@ const SignUp = () =>{
             <label>Password</label>
             <input type='password' value={password} onChange={e => setPassword(e.target.value)} className='form-control'/>
         </div>
-
-        {errors.length > 0 && (<div className='alert alert-danger'>
+        {errors}
+        {/* {errors.length > 0 && (<div className='alert alert-danger'>
         <h4>Ooopss.....</h4>
         <ul className='my-0'>
         {errors.map(err => <li key={err.message}>{err.message}</li>)}
         </ul>
-        </div>)}
+        </div>)} */}
         <button className='btn btn-primary'>Sign Up</button>
 </form>
     )
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,20 @@
-const Landing = () =>{
-    return <h1>Landing</h1>
+import axios from "axios";
+//We cant use hook here . hook is used in react component . getInitialProps is not a function
+
+const LandingPage = ({currentUser}) =>{
+    console.log(currentUser)
+    return <h1>LandingPage</h1>
+};
+
+//This is only in next js. So next js is going to call this function while it is attempting to render our application on the server. Get Initial props is our opportunity to attempt to fetch some data that this component needs during server side rendering process
+//This is static method
+LandingPage.getInitialProps = async() =>{
+    const response = await axios.get('/api/users/currentuser').catch((err) =>{
+        console.log(err.message)
+    });
+    console.log('I am on server');
+
+    return {};
+
 }
-export default Landing;
+export default LandingPage;
