9a59fcb3bf4c85831bc8b180ca4cfb52a68c24c9
Satyam1212 <satyamjadhav088@gmail.com>
2024-04-04 00:13:52 +0530

    Created Ticket service and Create docker image and also created pods and services

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+//No need of body-parser from version 4.16
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError } from '@sbjtickets/common';
+// import { NotFoundError } from './errors/not-found-error';
+
+export const app = express()
+app.set('trust proxy', true)//reason : traffic is being proximate to our application through ingress and nginx
+app.use(express.json())
+app.use(
+    cookieSession({
+        signed: false,//We are going to disabled encryption on this cookie because jwt already encrypted
+        secure: process.env.NODE_ENV !== 'test',//Cookie only used when user visiting our application over HTTPS connection
+
+    })
+)
+
+app.all('*', async () => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
